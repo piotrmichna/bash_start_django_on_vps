f7cc68bc8a6b76d661bf76f6bfc23fb50b7b38e1
Piotr Michna <poczta@piotrmichna.pl>
2021-03-08 13:45:40 +0100

    definicje zmiennych konfiguracyjnych

--- config_script.sh
+++ config_script.sh
@@ -2,3 +2,14 @@
 
 # author: Piotr Michna
 # e-mail: pm@piotrmichna.pl
+
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+BLUE="\e[0;34m"
+GREY="\e[0;90m"
+BLINK="\e[5m"
+NC="\e[0m"
+currentDate=$(date +"%F")
+currentTime=$(date +"%T")
+
+LOG_FILE="app.log"
