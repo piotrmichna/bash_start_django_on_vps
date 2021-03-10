216c4c06011be66aa841ab0c414cf6bf8242f9bf
Piotr Michna <poczta@piotrmichna.pl>
2021-03-10 13:23:16 +0100

    instukcje aktualizacji stytmeu

--- install_djanogo_on_vps.sh
+++ install_djanogo_on_vps.sh
@@ -3,3 +3,24 @@
 # author: Piotr Michna
 # e-mail: pm@piotrmichna.pl
 
+source config_script.sh
+
+sudo ls > /dev/null
+
+echo -ne "\n\r${C_TIT}${BOLD}------> INSTALL DJANGO <-----------[ ${currentDate} ${currentTime} ]${NC}\n\r"
+echo "" |& tee -a $LOG_FILE &> /dev/null
+echo "------> INSTALL DJANGO <-----------[ ${currentDate} ${currentTime} ]" |& tee -a $LOG_FILE &> /dev/null
+
+get_config
+
+if [ $C_TOOLS -eq 1 ] ; then
+    message 'Aktualizacja repozytorium' "-m"
+    sudo apt-get update |& tee -a $LOG_FILE &> /dev/null
+    message 'Wykonane' "-c"
+
+    message 'Aktualizacja systemu' "-m"
+    sudo apt-get upgrade -y |& tee -a $LOG_FILE &> /dev/null
+    message 'Wykonane' "-c"
+    message 'INSTALACJA NARZĘDZI' "-m"
+    
+fi
