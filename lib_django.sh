e21b494ed4b4e84ea976eac9c70f199cbd478f21
Piotr Michna <poczta@piotrmichna.pl>
2021-03-11 14:33:32 +0100

    Funkcja konfiguracji Django

--- lib_django.sh
+++ lib_django.sh
@@ -3,25 +3,7 @@
 # author: Piotr Michna
 # e-mail: pm@piotrmichna.pl
 
-function get_firtualenv(){
-    message "ŚRODOWISKO VIRTUALENV" "-t"
-
-    get_pip_install virtualenv
-
-    message 'Konfiguracja środowiska virtualenv'
-    cd $HOME/$PROJ_DIR
-    virtualenv -p python3 venv |& tee -a $LOG_FILE &> /dev/null
-    if [ -d "venv" ] ; then
-        message "Utworzenie środowiska virtualenv." "-c"
-        cd $HOME/$PROJ_DIR
-        . venv/bin/activate
-        message 'Aktywacja środowiska virtualenv' "-c"
-        message 'Instalacja wymaganych bibliotek' "-m"
-        get_pip_install psycopg2-binary Django django-rest
-    else
-        message "Nie udane utworzenie środowiska virtualenv." "-e"
-    fi
-}
+PYTHON_INSTAL_LIB="psycopg2-binary Django django-rest gunicorn"
 
 function get_pip_install(){
     for i in $@ ; do
@@ -42,7 +24,60 @@ function get_pip_install(){
     done
 }
 
+function get_firtualenv(){
+    message "ŚRODOWISKO VIRTUALENV" "-t"
+
+    get_pip_install virtualenv
+
+    message 'Konfiguracja środowiska virtualenv'
+    cd $HOME/$PROJ_DIR
+    virtualenv -p python3 venv |& tee -a $LOG_FILE &> /dev/null
+    if [ -d "venv" ] ; then
+        message "Utworzenie środowiska virtualenv." "-c"
+        cd $HOME/$PROJ_DIR
+        . venv/bin/activate
+        message 'Aktywacja środowiska virtualenv' "-c"
+        message 'Instalacja wymaganych bibliotek' "-m"
+        get_pip_install psycopg2-binary Django django-rest
+    else
+        message "Nie udane utworzenie środowiska virtualenv." "-e"
+    fi
+}
+
+function get_django(){
+    mkdir $HOME/$PROJ_DIR
+    if [ $? -eq 0 ] ; then
+        message "Utworzono katalog projektu $HOME/$PROJ_DIR." "-c"
+    else
+        message "Nie utoworzono $HOME/$PROJ_DIR." "-w"
+    fi
+
+    if [ $C_CGIT -eq 1 ]; then
+        git clone $GIT_LINK $HOME/$PROJ_DIR  |& tee -a $LOG_FILE &> /dev/null
+
+        if [ $? -eq 0 ] ; then
+            message "Pomyślnie pobrano repozytorium $GIT_LINK." "-c"
+            get_firtualenv
+        else
+            message "Pobieranie repozytorium $GIT_LINK." "-e"
+        fi
+    else
+        cd $HOME/$PROJ_DIR
+        git init
+        if [ $? -eq 0 ] ; then
+            message "Pomyślnie zinicjowano puste repozytorium w katalogu $HOME/$PROJ_DIR." "-c"
+            get_firtualenv
+            message "Django budowanie projektu." "-T"
+            cd $HOME/$PROJ_DIR
+            django-admin startproject $DJANGO_DIR
+            message "django-admin startproject $DJANGO_DIR." "-c"
+        else
+            message "Nie udana inicjalizacja repozytorium w katalogu $HOME/$PROJ_DIR." "-e"
+        fi        
+    fi
+}
 
 if [ "$0" == "./lib_django.sh" ] || [ "$0" == "lib_django.sh" ] ; then
     echo "Skrypt z lib_django.sh"
+    
 fi
