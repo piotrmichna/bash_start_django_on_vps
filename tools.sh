5339c7fd839a4e036ee8876808fa3eabc3e304fd
Piotr Michna <poczta@piotrmichna.pl>
2021-03-13 23:56:50 +0100

    definicje kolorystyki terminala

--- tools.sh
+++ tools.sh
@@ -3,3 +3,14 @@
 # author: Piotr Michna
 # e-mail: pm@piotrmichna.pl
 
+C_ERR="\e[0;31m"
+C_WOR="\e[0;33m"
+C_MES="\e[0;34m"
+C_QST="\e[0;32m"
+C_COR="\e[0;32m"
+C_TIT="\e[0;34m"
+C_NRM="\e[0;97m"
+GREEN="\e[0;32m"
+BLINK="\e[5m"
+BOLD="\e[1m"
+NC="\e[0m"
