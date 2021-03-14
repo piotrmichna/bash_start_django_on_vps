452ae759d172ec8b696fd9367e3cbb1ac76e45b4
Piotr Michna <poczta@piotrmichna.pl>
2021-03-14 10:43:07 +0100

    Rozbudowa funkcji startu skryptu

--- tools.sh
+++ tools.sh
@@ -11,6 +11,8 @@ C_COR="\e[0;32m"
 C_TIT="\e[0;34m"
 C_NRM="\e[0;97m"
 GREEN="\e[0;32m"
+INV="\e[7m"
+DM="\e[2m"
 BLINK="\e[5m"
 BOLD="\e[1m"
 NC="\e[0m"
@@ -23,7 +25,43 @@ LOG_FILE="$DIR_SC/log_${currentDate}_${currentTime}.log"
 
 function start_scripts(){
     sudo ls > /dev/null
-    echo -ne "\n\r${C_TIT}${BOLD}------> INSTALL DJANGO ON VPS <-----------[ ${currentDate} ${currentTime} ]${NC}\n\r"
+    echo -ne "\n\r${NC}${C_MES}${DM}-----------------------------------------------------------------"
+    echo "-----------------------------------------------------------------" |& tee -a $LOG_FILE &> /dev/null
+    echo -ne "\n\r${C_TIT}${BOLD}"
+    figlet -t -k -f /usr/share/figlet/small.flf " Django  on VPS " |& tee -a $LOG_FILE
+    echo -ne "${NC}${C_MES}${DM}-----------------------------------------------------------------"
+    echo "-----------------------------------------------------------------" |& tee -a $LOG_FILE &> /dev/null
+
+    echo -ne "\n\r${C_MES}  Autor: ${BOLD}Piotr Michna${NC}"
+    echo -ne "\n\r${C_MES}${DM} e-mail: pm@piotrmichna.pl"
+    echo -ne "\n\r${C_MES}${DM}   Data: 15.03.2021\n\r"
+
+    echo -ne "\n\r${C_MES}${DM} Skrypt przygotowany w oparciu o wirtualny serwer projektu:"
+    echo -ne "\n\r${C_MES} UW-TEAM.ORG Jakuba Mrugalskiego"
+    echo -ne "\n\r${C_MES}${DM}        Link: ${NC}${C_MES}https://mikr.us${NC}\n\r"
+    echo -ne "\n\r${C_MES}         MIKR.US 1.0 ${BLINK}35zł/rok"
+    echo -ne "\n\r${C_MES}${DM}         RAM: ${NC}${C_MES}256MB"
+    echo -ne "\n\r${C_MES}${DM} Technologia: ${NC}${C_MES}OpenVZ 6${NC}"
+    echo -ne "\n\r${C_MES}${DM}      System: ${NC}${C_MES}Ubuntu 16${NC}\n\r"
+    echo -ne "\n\r${C_MES}     Korzystając z tego linku https://mikr.us/?r=758803ea"
+    echo -ne "\n\r${C_MES}             otrzymasz dodatkowy miesiąc gratis.\n\r"
+
+    echo "  Autor: Piotr Michna" |& tee -a $LOG_FILE &> /dev/null
+    echo " e-mail: pm@piotrmichna.pl" |& tee -a $LOG_FILE &> /dev/null
+    echo "   Data: 15.03.2021" |& tee -a $LOG_FILE &> /dev/null
     echo "" |& tee -a $LOG_FILE &> /dev/null
-    echo "------> INSTALL DJANGO ON VPS <-----------[ ${currentDate} ${currentTime} ]" |& tee -a $LOG_FILE &> /dev/null
+    echo " Skrypt przygotowany w oparciu o wirtualny serwer projektu:" |& tee -a $LOG_FILE &> /dev/null
+    echo " UW-TEAM.ORG Jakuba Mrugalskiego" |& tee -a $LOG_FILE &> /dev/null
+    echo "        Link: https://mikr.us" |& tee -a $LOG_FILE &> /dev/null
+    echo "" |& tee -a $LOG_FILE &> /dev/null
+    echo "         MIKR.US 1.0 35zł/rok" |& tee -a $LOG_FILE &> /dev/null
+    echo "         RAM: 256MB" |& tee -a $LOG_FILE &> /dev/null
+    echo " Technologia: OpenVZ 6" |& tee -a $LOG_FILE &> /dev/null
+    echo "      System: Ubuntu 16" |& tee -a $LOG_FILE &> /dev/null
+    echo "" |& tee -a $LOG_FILE &> /dev/null
+    echo "     Korzystając z tego linku https://mikr.us/?r=758803ea" |& tee -a $LOG_FILE &> /dev/null
+    echo "             otrzymasz dodatkowy miesiąc gratis." |& tee -a $LOG_FILE &> /dev/null
+    echo "" |& tee -a $LOG_FILE &> /dev/null
 }
+
+start_scripts
