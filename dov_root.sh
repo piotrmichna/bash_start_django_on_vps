02265deeb08181a5fcdccf71e5bea31232949dbc
Piotr <pm@piotrmichna.pl>
2021-03-21 19:10:47 +0100

    instalacja virtualenv z poziomu root

--- dov_root.sh
+++ dov_root.sh
@@ -50,6 +50,7 @@ function get_required_install_tools(){
     message "AKTUALIZACJA I INSTALACJA" "-t"
     install_prog vim git links curl bc python3 python3-dev python3-pip
     pip3 install --upgrade pip
+    pip3 install virtualenv
     if [ ! -z "$1" ] ; then
         get_param 'Wybierz [x] aby zakończyć' "Xx"
     fi
